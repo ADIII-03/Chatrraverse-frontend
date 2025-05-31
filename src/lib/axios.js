bfc1127f00bd6443bab89c51bc152469c61030ed
ADIII-03 <aradityaraman0518@gmail.com>
2025-05-31 20:16:42 +0530

    kk

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API || 'http://localhost:8000/api/v1',
+    baseURL: import.meta.env.VITE_API || 'http://localhost:5001/api/v1',
     withCredentials: true ,
     
 });
