1df1a5530dc0839c68483b1e81ce5a247cca9430
Aditya Raman <aradityaraman0518@gmail.com>
2025-06-01 13:49:13 +0530

    Update useLogin.js

--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -3,17 +3,18 @@ import { login } from "../lib/api";
 
 const useLogin = () => {
   const queryClient = useQueryClient();
+
   const { mutate, isPending, error } = useMutation({
     mutationFn: login,
-    onSuccess : () => {queryClient.invalidateQueries({ queryKey: ["authUser"] })
-   // console.log("Login successful!")},
-  //   onSuccess: (data) => {
-  //   console.log('Mutation successful!', data);
-  // },
-
-    
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["authUser"] });
+      // console.log("Login successful!");
+    },
+    // Optional: handle success data
+    // onSuccess: (data) => {
+    //   console.log('Mutation successful!', data);
+    // },
   });
-// console.log(mutate, isPending, error);
 
   return { error, isPending, loginMutation: mutate };
 };
