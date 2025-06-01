0829f13bc51a9cce9d4c6323287e2a3417e8550b
Aditya Raman <aradityaraman0518@gmail.com>
2025-06-01 19:55:26 +0530

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,3 +4,16 @@ export const axiosInstance = axios.create({
   baseURL: 'https://chattraverse-backend.onrender.com/api/',
   withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("chat-app-token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
